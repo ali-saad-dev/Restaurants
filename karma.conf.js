e2b776a2acd55e52b7728440b7c2292e5760829d
Ali Saad <asaad@snelstart.nl>
2021-09-30 13:50:11 +0200

    Init app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Restaurants'),
+      dir: require('path').join(__dirname, './coverage/Snelbezorg'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Restaurants'`, () => {
+  it(`should have as title 'Snelbezorg'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Restaurants');
+    expect(app.title).toEqual('Snelbezorg');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Restaurants app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Snelbezorg app is running!');
   });
 });
