fcb811a0ef7989908fa73f2b59a3386ea3421669
Ali Saad <asaad@snelstart.nl>
2021-09-30 14:06:10 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { ProductsComponent } from './products/products.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantsComponent,
+    ProductsComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
